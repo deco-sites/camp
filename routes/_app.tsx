c0d528548afc94598ad7f57ae9538ec736b5e4a5
Marcos Candeia <marrcooos@gmail.com>
2023-07-24 15:21:19 -0300

    Import std
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,12 +1,9 @@
 import { AppProps } from "$fresh/server.ts";
-import { context } from "$live/live.ts";
-import GoogleTagManager from "partytown/integrations/GoogleTagManager.tsx";
 
 export default function App(props: AppProps) {
   return (
     <>
       {/* Add Tag Manager script during production only. To test it locally remove the condition */}
-      {!!context.deploymentId && <GoogleTagManager trackingId="G-5KHCG36F89" />}
       <props.Component />
     </>
   );
